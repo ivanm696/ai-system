692bd539012de6b19765fc28f888a993dd925197
ivanm696 <59553806+ivanm696@users.noreply.github.com>
2026-06-17 10:08:43 +0300

    fix(security): Arbitrary code execution vulnerability due to dynamic module

--- main.py
+++ main.py
@@ -1,3 +1,1 @@
-spec = importlib.util.spec_from_file_location('tasks.doc_generator', 'tasks/doc_generator.py')
-tasks.doc_generator = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(tasks.doc_generator)
+import importlib.util; spec = importlib.util.find_spec('tasks.doc_generator'); if spec: tasks.doc_generator = importlib.util.module_from_spec(spec); spec.loader.exec_module(tasks.doc_generator)
